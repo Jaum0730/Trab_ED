cb4e6ad054001413a266f4aafd02109d90111f82
Jaum0730 <64561972+Jaum0730@users.noreply.github.com>
2022-11-29 23:25:16 -0300

    Add files via upload
    Esse é o código principal

---
+++ Trab_Final.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Aluno{
+	//Data set
+	char * nome;
+	char * email;
+	char * endereco;
+	char * telefone;
+	int * matricula;
+	//struct of descendence
+	Aluno *fi_dir;
+	Aluno *fi_esq;
+
+}Aluno;
+
+Aluno * raiz = Null;
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
