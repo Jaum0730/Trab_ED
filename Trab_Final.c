1545c824638227d4e757c3b08ae3923fc224f0d5
Jaum0730 <lopesjoaovictor449@gmail.com>
2022-12-05 12:35:09 -0300

    Nova struct e Excluir

--- Trab_Final.c
+++ Trab_Final.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+
 typedef struct No{
-	int matricula;
-	//Data set
-	char * nome;
-	char * email;
-	char * endereco;
-	char * telefone;	
 	
-	struct No* fi_dir;
+	int matricula;
+	
+	struct Aluno_set{
+	char *nome[1]; 
+	char *email[2];
+	char *telefone[2];
+	char *endereco[2];
+}aluno;
+	struct Disciplinas{
+	char *disciplinas[10];
+	char *code[10];
+	int *nota_final[10];
+}disciplina;
+	
 	struct No* fi_esq;
+	struct No* fi_dir;
 
 
 
 }NO;
+
 NO * raiz1 = NULL;
 NO * raiz2 = NULL;
 
 //A variavel matricula corresponde a matricula da struct NO
 //o aux contera a raiz de busca 
+//A funcao "Ler" faz o papel de busca
 NO* Ler(int matricula, NO* aux){
     if(aux == NULL){
         return NULL; //vazia
@@ -46,21 +60,22 @@ NO* Ler(int matricula, NO* aux){
     }
 }
 
-void Cadastro(int matricula,char*nome,char*endereço,char*telefone,char*email, NO* raiz){
+void Cadastro(int matricula,char*nome,char*endereco,char*telefone,char*email, NO* raiz){
     NO* resp = Ler(matricula, raiz);
     if(resp == NULL || resp->matricula != matricula){// vazia ou eu posso adicionar
         NO* NEW = malloc (sizeof(NO));
         NEW->matricula = matricula;
-        NEW->nome = nome;
-        NEW->nome = endereço;
-        NEW->nome = telefone;
-        NEW->nome = email;
+        NEW->aluno.nome[0] = nome;
+        NEW->aluno.endereco[0] = endereco;
+        NEW->aluno.telefone[0] = telefone;
+        NEW->aluno.email[0] = email;
         NEW->fi_esq = NULL;
         NEW->fi_dir = NULL;
 
         if(resp == NULL){ //add na raiz
             raiz = NEW;
-        }else{
+        }
+        else{
             if(matricula < resp->matricula){
                 resp->fi_esq = NEW;
             }else{
@@ -73,6 +88,69 @@ void Cadastro(int matricula,char*nome,char*endereço,char*telefone,char*email, N
     }
 
 }
+
+ 
+NO* Excluir( NO*pt ,int matricula){
+    
+	
+	//caso da lista está vazia
+    if(pt==NULL){
+        return NULL;
+    //caso de ser uma folha ou raiz   
+    }else if(pt->fi_esq==NULL && pt->fi_dir==NULL){
+        if(pt->matricula > matricula){
+            pt->fi_esq = Excluir(pt->fi_esq, matricula);
+        }
+        else{
+             pt->pai->fi_esq = NULL;
+        }
+    //caso de estar no lado direito
+    }else if(pt->fi_dir !=NULL && pt->fi_esq == NULL){
+        if(pt->matricula > pt->pai->matricula){
+            pt->pai->fi_dir = pt->fi_dir;
+        }
+        else{
+             pt->pai->fi_esq = pt->fi_dir;
+        }
+
+    }else if(pt->fi_dir ==NULL && pt->fi_esq != NULL){
+          if(pt->matricula > pt->pai->matricula){
+            pt->pai->fi_dir = pt->fi_esq;
+        }else{
+             pt->pai->fi_esq = pt->fi_esq;
+        }
+    }else{
+        NO* aux = pt;
+       while (aux->fi_dir != NULL) {
+          aux = aux->fi_dir;
+       }
+        pt->matricula = aux->matricula;
+        if(pt->fi_dir->matricula == pt->matricula){
+            pt->fi_dir = NULL;
+        }
+        aux->pai->fi_dir= NULL;
+        if(aux ->fi_esq != NULL){
+            NO* aux2 = busca(aux->fi_esq->matricula, raiz1);
+            aux2->fi_dir = aux->fi_esq;
+        }
+    }
+
+    return pt;
+}
+
+void in_ordem(NO* aux){
+
+    if(aux->fi_esq = NULL){
+        in_ordem(aux->fi_esq);
+    }
+
+    printf("%d\t" , aux->matricula);
+
+    if(aux->fi_dir != NULL){
+        in_ordem(aux->fi_dir);
+    }
+}
+
 
 
 int main(){
